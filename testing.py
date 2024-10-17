217ddd56f853f2fa84f25cfb6fcccd0656998d2d
Michele Scarpiniti <michele.scarpiniti@uniroma1.it>
2024-10-17 16:50:31 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -4,7 +4,7 @@ This file contains main code for implementing the test phase for the
 identification of arrhythmia in ECGs, proposed in [1].
 
 [1] M. Scarpiniti, "Arrhythmia detection by data fusion of ECG scalograms and
-phasograms", submitted to *Expert Systems With Applications*, 2024.
+phasograms", submitted to *Sensors*, 2024.
 
 
 Created on Thu Jul 11 14:17:49 2024
